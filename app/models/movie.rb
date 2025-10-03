a9cd806e14ec06fe08ce604bec8b479eb1df9c15
rh3128 Raul Hinojos <rh3128@columbia.edu>
2025-10-03 06:23:42 +0000

    Fixed part 1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ActiveRecord::Base
+  ALL_RATINGS = %w[G PG PG-13 RNC-17].freeze
+
+  def self.all_ratings
+    ALL_RATINGS
+  end 
+
+  def self.with_ratings(ratings_list)
+    return all if ratings_list.blank?
+    where(rating: ratings_list)
+  end
 end
